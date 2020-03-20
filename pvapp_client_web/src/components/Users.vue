fbe7af4fd32ef3b2ac9833b1955fb8f7cdce77dc
CAO <CAO@Casals.local>
2020-03-20 15:30:24 +0100

    creation du composant User

---
+++ pvapp_client_web/src/components/Users.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-card max-width="95%" class="mx-auto mb-10">
+    <v-data-table :headers="headers" :items="desserts" item-key="name" group-by="group"></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Users",
+  data() {
+    return {
+      headers: [
+        {
+          text: "Dessert (100g serving)",
+          align: "start",
+          value: "name"
+        },
+        { text: "Ici", value: "group" }
+      ],
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          group: "Ice cream"
+        },
+        {
+          name: "Ice cream sandwich",
+          group: "Ice cream"
+        },
+        {
+          name: "Eclair",
+          group: "Cookie"
+        },
+        {
+          name: "Cupcake",
+          group: "Pastry"
+        },
+        {
+          name: "Gingerbread",
+          group: "Cookie"
+        },
+        {
+          name: "Jelly bean",
+          group: "Candy"
+        },
+        {
+          name: "Lollipop",
+          group: "Candy"
+        },
+        {
+          name: "Honeycomb",
+          group: "Toffee"
+        },
+        {
+          name: "Donut",
+          group: "Pastry"
+        },
+        {
+          name: "KitKat",
+          group: "Candy"
+        }
+      ]
+    };
+  }
+};
+</script>
