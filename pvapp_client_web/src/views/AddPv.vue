f10ba81e3b95b6cddccc62f144528610f81fefc9
Baudouin Coupey PopOs <baudouin.coupey@gmail.com>
2020-05-11 18:40:44 +0200

    prettier

--- pvapp_client_web/src/views/AddPv.vue
+++ pvapp_client_web/src/views/AddPv.vue
@@ -6,17 +6,47 @@
           <v-col cols="6" lg="6">
             <v-menu v-model="menu1" :close-on-content-click="false" max-width="290">
               <template v-slot:activator="{ on }">
-                <v-text-field :value="computedDateFormattedMeetingDate" label="Jour de la réunion" readonly v-on="on" @click:clear="meeting_date_date = null" prepend-icon="mdi-calendar"></v-text-field>
+                <v-text-field
+                  :value="computedDateFormattedMeetingDate"
+                  label="Jour de la réunion"
+                  readonly
+                  v-on="on"
+                  @click:clear="meeting_date_date = null"
+                  prepend-icon="mdi-calendar"
+                ></v-text-field>
               </template>
               <v-date-picker v-model="meeting_date_date" @change="menu1 = false" locale="fr-fr"></v-date-picker>
             </v-menu>
           </v-col>
           <v-col cols="6" sm="6">
-            <v-menu ref="menu" v-model="menu2" :close-on-content-click="false" :nudge-right="40" :return-value.sync="meeting_date_time" transition="scale-transition" offset-y max-width="290px" min-width="290px">
+            <v-menu
+              ref="menu"
+              v-model="menu2"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              :return-value.sync="meeting_date_time"
+              transition="scale-transition"
+              offset-y
+              max-width="290px"
+              min-width="290px"
+            >
               <template v-slot:activator="{ on }">
-                <v-text-field v-model="meeting_date_time" label="Heure de la réunion" prepend-icon="mdi-clock-outline" readonly v-on="on"></v-text-field>
+                <v-text-field
+                  v-model="meeting_date_time"
+                  label="Heure de la réunion"
+                  prepend-icon="mdi-clock-outline"
+                  readonly
+                  v-on="on"
+                ></v-text-field>
               </template>
-              <v-time-picker v-if="menu2" v-model="meeting_date_time" format="24hr" full-width @click:minute="$refs.menu.save(meeting_date_time)" :allowed-minutes="allowedStep"></v-time-picker>
+              <v-time-picker
+                v-if="menu2"
+                v-model="meeting_date_time"
+                format="24hr"
+                full-width
+                @click:minute="$refs.menu.save(meeting_date_time)"
+                :allowed-minutes="allowedStep"
+              ></v-time-picker>
             </v-menu>
           </v-col>
           <v-col cols="12">
@@ -26,17 +56,49 @@
           <v-col cols="6" lg="6">
             <v-menu v-model="menu3" :close-on-content-click="false" max-width="290">
               <template v-slot:activator="{ on }">
-                <v-text-field :value="computedDateFormattedMMeetingNextDate" clearable label="Jour de la prochaine réunion" readonly v-on="on" @click:clear="meeting_next_date_date = null" prepend-icon="mdi-calendar"></v-text-field>
+                <v-text-field
+                  :value="computedDateFormattedMMeetingNextDate"
+                  clearable
+                  label="Jour de la prochaine réunion"
+                  readonly
+                  v-on="on"
+                  @click:clear="meeting_next_date_date = null"
+                  prepend-icon="mdi-calendar"
+                ></v-text-field>
               </template>
               <v-date-picker v-model="meeting_next_date_date" @change="menu3 = false" locale="fr-fr"></v-date-picker>
             </v-menu>
           </v-col>
           <v-col cols="6" sm="6">
-            <v-menu ref="menu" v-model="menu4" :close-on-content-click="false" :nudge-right="40" :return-value.sync="meeting_next_date_time" transition="scale-transition" offset-y max-width="290px" min-width="290px">
+            <v-menu
+              ref="menu"
+              v-model="menu4"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              :return-value.sync="meeting_next_date_time"
+              transition="scale-transition"
+              offset-y
+              max-width="290px"
+              min-width="290px"
+            >
               <template v-slot:activator="{ on }">
-                <v-text-field v-model="meeting_next_date_time" label="Heure de la prochaine réunion" prepend-icon="mdi-clock-outline" readonly v-on="on" clearable></v-text-field>
+                <v-text-field
+                  v-model="meeting_next_date_time"
+                  label="Heure de la prochaine réunion"
+                  prepend-icon="mdi-clock-outline"
+                  readonly
+                  v-on="on"
+                  clearable
+                ></v-text-field>
               </template>
-              <v-time-picker v-if="menu4" v-model="meeting_next_date_time" format="24hr" full-width @click:minute="$refs.menu.save(meeting_next_date_time)" :allowed-minutes="allowedStep"></v-time-picker>
+              <v-time-picker
+                v-if="menu4"
+                v-model="meeting_next_date_time"
+                format="24hr"
+                full-width
+                @click:minute="$refs.menu.save(meeting_next_date_time)"
+                :allowed-minutes="allowedStep"
+              ></v-time-picker>
             </v-menu>
           </v-col>
 
@@ -47,7 +109,14 @@
             <v-switch v-model="state" :label="state" role="switch" false-value="En cours" true-value="Terminé"></v-switch>
           </v-col>
           <v-col cols="12">
-            <v-combobox v-model="affair_id" :items="affairs" item-text="name" item-value="id_affair" label="Affaire" :rules="affairRules"></v-combobox>
+            <v-combobox
+              v-model="affair_id"
+              :items="affairs"
+              item-text="name"
+              item-value="id_affair"
+              label="Affaire"
+              :rules="affairRules"
+            ></v-combobox>
           </v-col>
         </v-row>
       </v-container>
@@ -61,12 +130,9 @@
           Vous n'avez pas encore d'affaire <br />
           Veuillez commancer par en créer une
         </v-alert>
-
       </v-row>
       <v-row justify="center">
-        <v-btn color="success darken-1" dark @click="createAffair">
-          <v-icon left>mdi-pencil</v-icon>Créer une affaire
-        </v-btn>
+        <v-btn color="success darken-1" dark @click="createAffair"> <v-icon left>mdi-pencil</v-icon>Créer une affaire </v-btn>
       </v-row>
     </div>
   </div>
@@ -105,10 +171,7 @@ export default {
       meeting_next_place: "",
       affair_id: "",
       affairRules: [v => !!v || "Requis"],
-      addressRules: [
-        v => !!v || "Requis",
-        v => (v && v.length >= 10) || "Doit être supérieur à 10 caractères"
-      ]
+      addressRules: [v => !!v || "Requis", v => (v && v.length >= 10) || "Doit être supérieur à 10 caractères"]
     };
   },
   methods: {
@@ -117,17 +180,12 @@ export default {
     },
     validate() {
       if (this.meeting_next_date_date == "empty string") {
-        this.meeting_next_date =
-          this.meeting_next_date_date +
-          " " +
-          this.meeting_next_date_time +
-          ":00";
+        this.meeting_next_date = this.meeting_next_date_date + " " + this.meeting_next_date_time + ":00";
       } else {
         this.meeting_next_date = null;
       }
       let pvData = {
-        meeting_date:
-          this.meeting_date_date + " " + this.meeting_date_time + ":00",
+        meeting_date: this.meeting_date_date + " " + this.meeting_date_time + ":00",
         meeting_place: this.meeting_place,
         meeting_next_date: this.meeting_next_date,
         meeting_next_place: this.meeting_next_place,
@@ -175,14 +233,10 @@ export default {
       userId: "userId"
     }),
     computedDateFormattedMeetingDate() {
-      return this.meeting_date_date
-        ? moment(this.meeting_date_date).format("dddd LL")
-        : "";
+      return this.meeting_date_date ? moment(this.meeting_date_date).format("dddd LL") : "";
     },
     computedDateFormattedMMeetingNextDate() {
-      return this.meeting_next_date_date
-        ? moment(this.meeting_next_date_date).format("dddd LL")
-        : "";
+      return this.meeting_next_date_date ? moment(this.meeting_next_date_date).format("dddd LL") : "";
     }
   }
 };
