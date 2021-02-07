95f80cff3103b2eb22ea3e2450c0bab9b4ab9587
Baudouin Coupey PopOs <baudouin.coupey@gmail.com>
2021-02-07 22:06:27 +0100

    New version 0.1.4-beta

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,10 +5,26 @@
       <v-avatar left>
         <v-icon>mdi-checkbox-marked-circle</v-icon>
       </v-avatar>
-      Version 0.1.3-beta
+      Version {{ appVersion }}
     </v-chip>
 
-    <v-card class="mx-auto" color="primary" dark max-width="500">
+    <v-card class="mx-auto mb-5" color="primary" dark max-width="600">
+      <v-card-title>
+        <v-icon large left>
+          mdi-heart
+        </v-icon>
+        <span class="title font-weight-light">Nouveautés</span>
+      </v-card-title>
+
+      <v-card-text class="text-left">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item>Ajout des notifications de version</v-list-item>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card-text>
+    </v-card>
+    <v-card class="mx-auto" color="primary" dark max-width="600">
       <v-card-title>
         <v-icon large left>
           mdi-rocket-launch
@@ -19,13 +35,17 @@
       <v-card-text class="text-left">
         <v-list-item>
           <v-list-item-content>
-            <v-list-item>UX : Modification de la page A propos</v-list-item>
-            <v-list-item>UX : Changement de "Type de Réunion" pour "Phase"</v-list-item>
+            <v-list-item>UX : Ajout des propostions de groupe lors de la création de participants</v-list-item>
+            <v-list-item
+              >Suppression de l'obligation de renseigner un numéro de telephone et un mail lors de la création des participants</v-list-item
+            >
+            <v-list-item>Ajout de la possiblité de ne pas mettre de status. </v-list-item>
+            <v-list-item>Changement de du selecteur de status dans le formulaire et dans la page PV </v-list-item>
           </v-list-item-content>
         </v-list-item>
       </v-card-text>
     </v-card>
-    <v-card class="mx-auto mt-5" color="primary" dark max-width="500">
+    <v-card class="mx-auto mt-5" color="primary" dark max-width="600">
       <v-card-title>
         <v-icon large left>
           mdi-wrench
@@ -36,13 +56,50 @@
       <v-card-text class="text-left">
         <v-list-item>
           <v-list-item-content>
-            <v-list-item>Se supprimer soi-même dans les participants aux PV est désormais impossible</v-list-item>
+            <v-list-item>Ajout de la mise a jours des Status dans la page des PV.</v-list-item>
           </v-list-item-content>
         </v-list-item>
       </v-card-text>
     </v-card>
     <h3 class="mt-10 mb-5">Anciennes versions :</h3>
-    <v-card class="mx-auto text-left" color="cyan darken-1" dark max-width="500">
+    <v-card class="mx-auto text-left" color="cyan darken-1" dark max-width="600">
+      <v-chip class="ma-5" color="cyan darken-3" text-color="white">
+        <v-avatar left>
+          <v-icon>mdi-checkbox-marked-circle</v-icon>
+        </v-avatar>
+        Version 0.1.3-beta
+      </v-chip>
+      <v-card-title>
+        <v-icon left>
+          mdi-rocket-launch
+        </v-icon>
+        <span class="title font-weight-light">Améliorations</span>
+      </v-card-title>
+      <v-card-text>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item>UX : Modification de la page A propos</v-list-item>
+            <v-list-item>UX : Changement de "Type de Réunion" pour "Phase"</v-list-item>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card-text>
+
+      <v-card-title>
+        <v-icon left>
+          mdi-wrench
+        </v-icon>
+        <span class="title font-weight-light">Corrections</span>
+      </v-card-title>
+      <v-card-text>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item>Se supprimer soi-même dans les participants aux PV est désormais impossible</v-list-item>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
       <v-chip class="ma-5" color="cyan darken-3" text-color="white">
         <v-avatar left>
           <v-icon>mdi-checkbox-marked-circle</v-icon>
@@ -79,6 +136,7 @@
       </v-card-text>
 
       <v-divider class="mx-4"></v-divider>
+
       <v-chip class="ma-5" color="cyan darken-3" text-color="white">
         <v-avatar left>
           <v-icon>mdi-checkbox-marked-circle</v-icon>
@@ -119,3 +177,14 @@
     </v-card>
   </div>
 </template>
+
+<script>
+import { version } from "../../package";
+export default {
+  data() {
+    return {
+      appVersion: version
+    };
+  }
+};
+</script>
