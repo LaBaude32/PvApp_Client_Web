d74c9dc7af77abf8fe2f409db0be1d75df325048
Baudouin Coupey <baudouin.coupey@ik.me>
2024-03-10 14:25:58 +0100

    🐛 FIX bouton signaler un bug apparait en cas de succès

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -2,7 +2,7 @@
   <v-snackbar :value="state" top right :color="type" app>
     <v-icon :color="btnColor" :dark="isDark" @click="close">mdi-close-circle</v-icon>
     {{ text }}
-    <v-btn class="ml-2" target="_blank" href="https://github.com/LaBaude32/PvApp_Client_Web/issues">Signaler un bug</v-btn>
+    <v-btn v-if="type == 'error'" class="ml-2" target="_blank" href="https://github.com/LaBaude32/PvApp_Client_Web/issues">Signaler un bug</v-btn>
   </v-snackbar>
 </template>
 
