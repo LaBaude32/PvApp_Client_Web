1da47b4f27472707a4104f37d3c77e576e7992d5
Baudouin Coupey <baudouin.coupey@ik.me>
2024-03-10 14:23:02 +0100

    ✨ Ajout d'un bouton pour signaler les bugs

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,7 +1,8 @@
 <template>
   <v-snackbar :value="state" top right :color="type" app>
-    {{ text }}
     <v-icon :color="btnColor" :dark="isDark" @click="close">mdi-close-circle</v-icon>
+    {{ text }}
+    <v-btn class="ml-2" target="_blank" href="https://github.com/LaBaude32/PvApp_Client_Web/issues">Signaler un bug</v-btn>
   </v-snackbar>
 </template>
 
