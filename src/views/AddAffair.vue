ad010264f76998d8b3e1013fb401ac296c9502e2
Baudouin Coupey <baudouin.coupey@ik.me>
2024-03-17 19:24:13 +0100

    ♻️ vue 3 addAffair Page

--- src/views/AddAffair.vue
+++ src/views/AddAffair.vue
@@ -24,38 +24,40 @@
           </v-col>
         </v-row>
       </v-container>
-      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate"> Valider </v-btn>
-      <v-btn color="error" class="mr-4" @click="reset"> Vider </v-btn>
+      <v-container class="text-center">
+        <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Valider</v-btn>
+        <v-btn color="error" class="mr-4" @click="reset">Vider</v-btn>
+      </v-container>
     </v-form>
   </div>
 </template>
 
 <script>
-import Axios from "axios";
-import routesCONST from "../utilities/constantes";
-import { DateTime, Settings } from "luxon";
-Settings.defaultLocale = "fr";
+import Axios from 'axios'
+import routesCONST from '../utilities/constantes'
+import { DateTime, Settings } from 'luxon'
+Settings.defaultLocale = 'fr'
 
 export default {
   data: () => ({
     valid: false,
     nameRules: [
-      (v) => !!v || "Requis",
-      (v) => (v && v.length <= 50) || "Doit être inferieur à 50 caractères",
-      (v) => (v && v.length >= 10) || "Doit être supérieur à 10 caractères"
+      (v) => !!v || 'Requis',
+      (v) => (v && v.length <= 50) || 'Doit être inferieur à 50 caractères',
+      (v) => (v && v.length >= 10) || 'Doit être supérieur à 10 caractères'
     ],
-    addressRules: [(v) => !!v || "Requis", (v) => (v && v.length >= 10) || "Doit être supérieur à 10 caractères"],
-    descriptionRules: [(v) => v.length <= 100 || "Doit être inferieur à 120 caractères"],
-    meetingRules: [(v) => !!v || "Requis", (v) => v == "Chantier" || v == "Etude" || "Choisir dans la liste"],
-    name: "",
-    address: "",
-    meetingType: "",
-    description: "",
-    items: ["Chantier", "Etude"]
+    addressRules: [(v) => !!v || 'Requis', (v) => (v && v.length >= 10) || 'Doit être supérieur à 10 caractères'],
+    descriptionRules: [(v) => v.length <= 100 || 'Doit être inferieur à 120 caractères'],
+    meetingRules: [(v) => !!v || 'Requis', (v) => v == 'Chantier' || v == 'Etude' || 'Choisir dans la liste'],
+    name: '',
+    address: '',
+    meetingType: '',
+    description: '',
+    items: ['Chantier', 'Etude']
   }),
   computed: {
     id() {
-      return this.$store.state.user.userId;
+      return this.$store.state.user.userId
     }
   },
   methods: {
@@ -66,19 +68,19 @@ export default {
         address: this.address,
         meetingType: this.meetingType,
         progress: 0
-      };
-      Axios.post("/affairs", affairData)
+      }
+      Axios.post('/affairs', affairData)
         .then((response) => {
           if (response.status == 201) {
-            let affairId = response.data.affairId;
+            let affairId = response.data.affairId
             let pvData = {
-              state: "En cours",
-              meetingPlace: "Indéfini",
-              meetingDate: new Date().toISOString().slice(0, 19).replace("T", " "),
+              state: 'En cours',
+              meetingPlace: 'Indéfini',
+              meetingDate: new Date().toISOString().slice(0, 19).replace('T', ' '),
               userId: this.$store.state.user.userId,
               affairId: affairId
-            };
-            Axios.post("/pvs", pvData)
+            }
+            Axios.post('/pvs', pvData)
               .then((response) => {
                 if (response.status == 201) {
                   // let pvId = response.data.id_pv;
@@ -102,21 +104,21 @@ export default {
                   this.$router.push({
                     name: routesCONST.affair.name,
                     params: { id: affairId }
-                  });
+                  })
                 }
               })
               .catch((error) => {
-                console.log(error);
-              });
+                console.log(error)
+              })
           }
         })
         .catch((error) => {
-          console.log(error);
-        });
+          console.log(error)
+        })
     },
     reset() {
-      this.$refs.form.reset();
+      this.$refs.form.reset()
     }
   }
-};
+}
 </script>
