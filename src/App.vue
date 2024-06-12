b0c7e0229f029a4c28111c2ceafd223cae49cde1
Baudouin Coupey <baudouin.coupey@ik.me>
2024-06-12 21:58:50 +0200

    🐛 FIX bouton de deconnection qui ne fonctionne pas

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
         </template>
         <v-list>
           <v-list-item v-for="(item, index) in items" :key="index" :value="index">
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title @click.prevent="action(item.path)">{{ item.title }}</v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
