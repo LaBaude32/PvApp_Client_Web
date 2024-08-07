66a33dd5a5d85a2e20479086f7fcf60681356775
Baudouin Coupey <baudouin.coupey@ik.me>
2024-08-07 23:44:49 +0200

    🚧 WIP refacto PV page

--- src/views/Pv.vue
+++ src/views/Pv.vue
@@ -17,7 +17,6 @@
       v-model:items="items"
       v-model:editedIndex="editedIndex"
       v-model:editedItem="editedItem"
-      v-model:formatedCompletionDate="formatedCompletionDate"
       :pvDetails="pvDetails"
       :pvUsers="pvUsers"
       :meetingType="meetingType"
@@ -40,294 +39,270 @@
   </div>
 </template>
 
-<script>
-import Items from '../components/Items.vue'
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+import Items from '@/components/Items.vue'
 import Users from '@/components/Users.vue'
 import ModalValidation from '@/components/ModalValidation.vue'
 import Axios from 'axios'
 import { getRouteName } from '../utilities/constantes'
-import { mapGetters } from 'vuex'
+import { useStore } from 'vuex'
+import { useRoute, useRouter } from 'vue-router'
 import { DateTime, Settings } from 'luxon'
 Settings.defaultLocale = 'fr'
 
-export default {
-  components: {
-    Users,
-    ModalValidation,
-    Items
+const store = useStore()
+const route = useRoute()
+const router = useRouter()
+
+const pvId = ref(Number)
+const dialog = ref(false)
+const ModalValidationDialog = ref(false)
+const meetingType = ref(null)
+const standardRequirement = [(v) => !!v || 'Requis']
+const pvDetails = ref(Object)
+const pvUsers = ref(Array)
+const pvConnectedParticipants = ref(Array)
+const items = ref(Array)
+const newImage = ref('')
+const headers = ref([
+  {
+    title: 'Position',
+    align: 'center',
+    value: 'position'
   },
-  data() {
-    return {
-      pvId: Number,
-      dialog: false,
-      ModalValidationDialog: false,
-      meetingType: undefined,
-      standardRequirement: [(v) => !!v || 'Requis'],
-      pvDetails: {},
-      pvUsers: [],
-      pvConnectedParticipants: [],
-      items: [],
-      newImage: 'lala',
-      headers: [
-        {
-          title: 'Position',
-          align: 'center',
-          value: 'position'
-        },
-        { title: 'Note', value: 'note', sortable: false },
-        { title: 'Suite à donner', value: 'followUp', sortable: false },
-        { title: 'Ressource', value: 'resources', sortable: false },
-        { title: 'Echeance', value: 'completion', sortable: false },
-        { title: "Date d'echéance", value: 'completionDate' },
-        { title: 'Visible', value: 'visible' },
-        { title: 'Photo', value: 'image' },
-        { title: 'Actions', value: 'actions', sortable: false }
-      ],
-      editedIndex: -1,
-      editedItem: {
-        position: Number,
-        lotsToReturn: [],
-        lots: [],
-        note: '',
-        followUp: '',
-        resources: '',
-        completion: '',
-        completionToReturn: null,
-        completionDate: '',
-        completionDateDate: '',
-        completionDateTime: '',
-        image: null,
-        visible: '',
-        isNewImage: false
-      },
-      defaultItem: {
-        position: Number,
-        lotsToReturn: [],
-        lots: [],
-        note: null,
-        followUp: null,
-        resources: null,
-        completion: ['A faire', 'Urgent', 'Fait'],
-        completionDate: '',
-        completionDateDate: '',
-        completionDateTime: '',
-        image: null,
-        visible: true,
-        isNewImage: false
-      }
-    }
-  },
+  { title: 'Note', value: 'note', sortable: false },
+  { title: 'Suite à donner', value: 'followUp', sortable: false },
+  { title: 'Ressource', value: 'resources', sortable: false },
+  { title: 'Echeance', value: 'completion', sortable: false },
+  { title: "Date d'echéance", value: 'completionDate' },
+  { title: 'Visible', value: 'visible' },
+  { title: 'Photo', value: 'image' },
+  { title: 'Actions', value: 'actions', sortable: false }
+])
+const editedIndex = ref(-1)
+const editedItem = ref({
+  position: Number,
+  lotsToReturn: [],
+  lots: [],
+  note: '',
+  followUp: '',
+  resources: '',
+  completion: '',
+  completionToReturn: null,
+  completionDate: '',
+  completionDateDate: '',
+  completionDateTime: '',
+  image: null,
+  visible: '',
+  isNewImage: false
+})
+const defaultItem = ref({
+  position: Number,
+  lotsToReturn: [],
+  lots: [],
+  note: null,
+  followUp: null,
+  resources: null,
+  completion: ['A faire', 'Urgent', 'Fait'],
+  completionDate: '',
+  completionDateDate: '',
+  completionDateTime: '',
+  image: null,
+  visible: true,
+  isNewImage: false
+})
 
-  computed: {
-    ...mapGetters('user', {
-      userId: 'userId'
-    }),
-    formTitle() {
-      return this.editedIndex === -1 ? 'Nouvel item' : "Modifier l'item"
-    },
-    formatedCompletionDate: {
-      get() {
-        return this.editedItem.completionDate
-          ? DateTime.fromSQL(this.editedItem.completionDate).toFormat('yyyy-LL-dd')
-          : ''
-      },
-      set(val) {
-        this.editedItem.completionDate = DateTime.fromSQL(val).toFormat('yyyy-LL-dd')
-      }
-    }
-  },
+const userId = computed(() => store.getters['user/userId'])
+const formTitle = computed(() => (editedIndex === -1 ? 'Nouvel item' : "Modifier l'item"))
 
-  mounted() {
-    this.getData()
-  },
+onMounted(() => {
+  getData()
+})
 
-  methods: {
-    async getData() {
-      this.pvId = this.$route.params.id
-      let dt = {
-        params: {
-          pvId: this.pvId,
-          userId: this.userId
-        }
-      }
-      let res = await Axios.get('pvs/pvId', dt)
-      if (res.data.items) {
-        this.items = [...res.data.items]
-      }
-      this.items.forEach((element) => {
-        element.visible == 1 ? (element.visible = true) : false
-      })
-      this.pvDetails = res.data.pv
-      this.pvUsers = res.data.users
-      this.pvConnectedParticipants = res.data.connectedParticipants
-      this.meetingType = res.data.pv.affairMeetingType
-      if (this.meetingType == 'Chantier') {
-        this.headers.splice(1, 0, { text: 'Lot', value: 'lots' })
-        // this.headers.splice(1, 0, { text: 'Lot', value: 'lots' })
-        this.defaultItem.lots = this.pvDetails.lots
-      }
-      this.$store.dispatch('affair/loadAffairByPv', this.pvDetails.affairId)
-    },
-
-    editItem(item) {
-      this.editedIndex = this.items.indexOf(item)
-      this.editedItem = { ...item }
-      this.editedItem.lotsToReturn = item.lots
-      this.pvDetails.lots ? (this.editedItem.lots = this.pvDetails.lots) : (this.editedItem = undefined)
-      this.editedItem.completionToReturn = item.completion
-      this.editedItem.completion = [item.completion]
-      this.editedItem.completion.push(...this.defaultItem.completion)
-      item.image ? (this.editedItem.isNewImage = false) : (this.editedItem.isNewImage = true)
-
-      this.dialog = true
-    },
-
-    deleteItem(item) {
-      const index = this.items.indexOf(item)
-      confirm('Etes-vous sûr de vouloir supprimer cet item?') &&
-        Axios.delete('items/itemId', { params: { itemId: item.itemId, pvId: this.pvId } })
-          .then((response) => {
-            if (response.status == 204) {
-              this.$store.dispatch('notification/success', "L'item à bien été supprimé")
-              this.items.splice(index, 1)
-            }
-          })
-          .catch((error) => {
-            this.$store.dispatch(
-              'notification/error',
-              `Erreur : l'item n'a pas été supprimé en base de donnée. ${error}`
-            )
-          })
-    },
-
-    close() {
-      this.dialog = false
-      this.editedItem = { ...this.defaultItem }
-      this.editedIndex = -1
-    },
-
-    save() {
-      this.editedItem.lots = this.editedItem.lotsToReturn
-      if (this.editedItem.completionDate == '' || this.editedItem.completionDate == 'Invalid date') {
-        this.editedItem.completionDate = null
-      }
-
-      const fd = new FormData()
-      fd.append('position', this.editedItem.position)
-      fd.append('note', this.editedItem.note)
-      fd.append('followUp', this.editedItem.followUp)
-      fd.append('resources', this.editedItem.resources)
-      fd.append('completion', this.editedItem.completionToReturn)
-      fd.append('completionDate', this.editedItem.completionDate)
-      fd.append('image', this.editedItem.image)
-      fd.append('thumbnail', null)
-      fd.append('visible', this.editedItem.visible)
-      fd.append('lots', null)
-      fd.append('pvId', this.pvId)
-
-      if (this.meetingType == 'Chantier' && this.editedItem.lotsToReturn) {
-        let lotTransit = []
-        this.editedItem.lotsToReturn.forEach((element) => {
-          lotTransit.push(element.lotId)
-        })
-        fd.set('lots', lotTransit)
-      }
-
-      if (this.editedIndex > -1) {
-        //EXISTING ITEM
-        fd.append('itemId', this.editedItem.itemId)
-        if (this.editedItem.image != null && this.editedItem.isNewImage == true) {
-          this.updateItem(fd)
-        }
-      } else {
-        //NEW ITEM
-        this.postItem(fd)
-      }
-      this.editedItem = { ...this.defaultItem }
-    },
-
-    postItem(fd) {
-      Axios.post('/items', fd)
-        .then((response) => {
-          if (response.status == 201) {
-            this.items.push(response.data)
-            this.close()
-            this.$store.dispatch('notification/success', "Ajout de l'item effectué")
-          } else {
-            console.log(response)
-            console.log(typeof response.data.itemId)
-          }
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    },
-
-    async updateItem(fd) {
-      //Upload new image
-      if (this.editedItem.image != null && this.editedItem.isNewImage == true) {
-        const fdImage = new FormData()
-        fdImage.append('itemId', this.editedItem.itemId)
-        fdImage.append('image', this.editedItem.image)
-        const res = await Axios.post('items/updateImage', fdImage)
-        fd.set('image', res.data.image)
-      }
-
-      let data = {}
-      fd.forEach((value, key) => (data[key] = value))
-      for (const iterator in data) {
-        if (data[iterator] == 'null') {
-          data[iterator] = null
-        }
-      }
-
-      Axios.put('items/itemId', data)
-        .then((response) => {
-          if (response.status == 200) {
-            this.editedItem.completion = this.editedItem.completionToReturn
-            Object.assign(this.items[this.editedIndex], data)
-            this.editedItem.completion = []
-            this.close()
-            this.$store.dispatch('notification/success', "Mise à jour de l'item effectué")
-          } else {
-            console.log(response)
-            console.log(typeof response.data.itemId)
-          }
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    },
-    changeVisible(item) {
-      let data = {
-        itemId: item.itemId,
-        visible: item.visible
-      }
-      Axios.put('items/itemId/visibility', data)
-        .then((response) => {
-          if (response.status == 200) {
-            this.$store.dispatch('notification/success', "L'item a été mis à jour")
-          }
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    },
-    pvValidation() {
-      Axios.put('pvs/pvId/validation', { pvId: this.pvId })
-        .then((response) => {
-          if (response.status == 204) {
-            this.$router.push({ name: getRouteName('finishedPv'), params: { id: this.pvId } })
-            this.ModalValidationDialog = false
-          }
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    },
-    returnToAffair() {
-      this.$router.push({ name: getRouteName('affair'), params: { id: this.pvDetails.affairId } })
+async function getData() {
+  pvId = route.params.id
+  let dt = {
+    params: {
+      pvId: pvId.value,
+      userId: userId.value
     }
   }
+  let res = await Axios.get('pvs/pvId', dt)
+  if (res.data.items) {
+    items.value = [...res.data.items]
+  }
+  items.value.forEach((element) => {
+    element.visible == 1 ? (element.visible = true) : false
+  })
+  pvDetails.value = res.data.pv
+  pvUsers.value = res.data.users
+  pvConnectedParticipants.value = res.data.connectedParticipants
+  meetingType.value = res.data.pv.affairMeetingType
+  if (meetingType.value == 'Chantier') {
+    headers.value.splice(1, 0, { text: 'Lot', value: 'lots' })
+    // headers.splice(1, 0, { text: 'Lot', value: 'lots' })
+    defaultItem.value.lots = pvDetails.lots
+  }
+  store.dispatch('affair/loadAffairByPv', pvDetails.value.affairId)
+}
+
+function editItem(item) {
+  editedIndex.value = items.value.indexOf(item)
+  editedItem.value = { ...item }
+  editedItem.value.lotsToReturn = item.lots
+  pvDetails.value.lots ? (editedItem.lots = pvDetails.lots) : (editedItem = undefined)
+  editedItem.value.completionToReturn = item.completion
+  editedItem.value.completion = [item.completion]
+  editedItem.value.completion.push(...defaultItem.completion)
+  item.image ? (editedItem.value.isNewImage = false) : (editedItem.value.isNewImage = true)
+
+  dialog = true
+}
+
+function deleteItem(item) {
+  const index = items.value.indexOf(item)
+  confirm('Etes-vous sûr de vouloir supprimer cet item?') &&
+    Axios.delete('items/itemId', { params: { itemId: item.itemId, pvId: pvId } })
+      .then((response) => {
+        if (response.status == 204) {
+          store.dispatch('notification/success', "L'item à bien été supprimé")
+          items.value.splice(index, 1)
+        }
+      })
+      .catch((error) => {
+        store.dispatch('notification/error', `Erreur : l'item n'a pas été supprimé en base de donnée. ${error}`)
+      })
+}
+
+function close() {
+  dialog = false
+  editedItem = { ...defaultItem }
+  editedIndex = -1
+}
+
+function save() {
+  editedItem.lots = editedItem.lotsToReturn
+  if (editedItem.completionDate == '' || editedItem.completionDate == 'Invalid date') {
+    editedItem.completionDate = null
+  }
+
+  const fd = new FormData()
+  fd.append('position', editedItem.position)
+  fd.append('note', editedItem.note)
+  fd.append('followUp', editedItem.followUp)
+  fd.append('resources', editedItem.resources)
+  fd.append('completion', editedItem.completionToReturn)
+  fd.append('completionDate', editedItem.completionDate)
+  fd.append('image', editedItem.image)
+  fd.append('thumbnail', null)
+  fd.append('visible', editedItem.visible)
+  fd.append('lots', null)
+  fd.append('pvId', pvId)
+
+  if (meetingType == 'Chantier' && editedItem.lotsToReturn) {
+    let lotTransit = []
+    editedItem.lotsToReturn.forEach((element) => {
+      lotTransit.push(element.lotId)
+    })
+    fd.set('lots', lotTransit)
+  }
+
+  if (editedIndex > -1) {
+    //EXISTING ITEM
+    fd.append('itemId', editedItem.itemId)
+    if (editedItem.image != null && editedItem.isNewImage == true) {
+      updateItem(fd)
+    }
+  } else {
+    //NEW ITEM
+    postItem(fd)
+  }
+  editedItem = { ...defaultItem }
+}
+
+function postItem(fd) {
+  Axios.post('/items', fd)
+    .then((response) => {
+      if (response.status == 201) {
+        items.push(response.data)
+        close()
+        $store.dispatch('notification/success', "Ajout de l'item effectué")
+      } else {
+        console.log(response)
+        console.log(typeof response.data.itemId)
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+async function updateItem(fd) {
+  //Upload new image
+  if (editedItem.image != null && editedItem.isNewImage == true) {
+    const fdImage = new FormData()
+    fdImage.append('itemId', editedItem.itemId)
+    fdImage.append('image', editedItem.image)
+    const res = await Axios.post('items/updateImage', fdImage)
+    fd.set('image', res.data.image)
+  }
+
+  let data = {}
+  fd.forEach((value, key) => (data[key] = value))
+  for (const iterator in data) {
+    if (data[iterator] == 'null') {
+      data[iterator] = null
+    }
+  }
+
+  Axios.put('items/itemId', data)
+    .then((response) => {
+      if (response.status == 200) {
+        editedItem.completion = editedItem.completionToReturn
+        Object.assign(items[editedIndex], data)
+        editedItem.completion = []
+        close()
+        $store.dispatch('notification/success', "Mise à jour de l'item effectué")
+      } else {
+        console.log(response)
+        console.log(typeof response.data.itemId)
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+function changeVisible(item) {
+  let data = {
+    itemId: item.itemId,
+    visible: item.visible
+  }
+  Axios.put('items/itemId/visibility', data)
+    .then((response) => {
+      if (response.status == 200) {
+        $store.dispatch('notification/success', "L'item a été mis à jour")
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+function pvValidation() {
+  Axios.put('pvs/pvId/validation', { pvId: pvId })
+    .then((response) => {
+      if (response.status == 204) {
+        router.push({ name: getRouteName('finishedPv'), params: { id: pvId } })
+        ModalValidationDialog = false
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+function returnToAffair() {
+  router.push({ name: getRouteName('affair'), params: { id: pvDetails.affairId } })
 }
 </script>
