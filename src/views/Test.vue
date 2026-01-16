9a5f24b72234488dcf8a0a0d31824ee2ea99b7e4
Baudouin Coupey Macbook <baudouin.coupey@ik.me>
2026-01-16 20:25:02 +0100

    🚧 création du FAB

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,25 +1,48 @@
 <template>
-  <TestComponent v-model:meetingDateDate="meetingDateDate" v-model:meetingDateTime="meetingDateTime" :validateForm="validate2" v-model:isFormValid="valid"/>
+  <v-fab :key="fabPosition" app :color="open ? '' : 'primary'" size="large" icon>
+    <v-icon>{{ open ? 'mdi-close' : 'mdi-plus' }}</v-icon>
+    <v-speed-dial
+      v-model="open"
+      location="top center"
+      transition="scale-transition"
+      activator="parent"
+    >
+      <v-btn key="1" color="primary" icon size="large">
+        <v-icon size="24">mdi-note-plus</v-icon>
+      </v-btn>
+
+      <v-btn key="2" color="tertiary" icon size="large">
+        <v-icon size="24">mdi-account-plus</v-icon>
+      </v-btn>
+
+      <v-btn key="3" color="tertiary" icon size="large">
+        <v-icon size="24">mdi-account-search</v-icon>
+      </v-btn>
+
+      <v-btn key="4" color="tertiary" icon size="large">
+        <v-icon size="24">mdi-qrcode-scan</v-icon>
+      </v-btn>
+    </v-speed-dial>
+  </v-fab>
 </template>
 
 <script setup>
-import { useDate } from 'vuetify'
-import { ref, onMounted, computed } from 'vue'
-import TestComponent from '../components/TestComponent.vue'
+import { shallowRef } from 'vue'
 
-const valid = ref(false)
-const date = useDate()
-const meetingDateDate = ref()
-const meetingDateTime = ref()
+const open = shallowRef(false)
+const fabPosition = shallowRef('absolute')
 
-function validate2() {
-  console.log(date.toISO(meetingDateDate.value))
-  console.log(meetingDateTime.value)
-}
-
-// const userId = computed(() => store.state.userId)
-
-const displayMeetingDate = computed(() => {
-  return meeting_date_date.value ? date.format(meeting_date_date.value, 'fullDate') : null
+defineProps({
+  addItem: Function,
+  addUser: Function,
+  searchUser: Function,
+  openQrCode: Function
 })
 </script>
+
+<style scoped>
+/* This is for documentation purposes and will not be needed in your application */
+::v-deep(.v-application__wrap) {
+  min-height: 0 !important;
+}
+</style>
